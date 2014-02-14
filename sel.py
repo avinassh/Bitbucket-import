cc19470195e1273107033d2bd0aed4c58dadf340
avinassh <avinashsajjan@gmail.com>
2014-02-14 17:40:39 +0530

    code cleanup

--- sel.py
+++ sel.py
@@ -1,35 +1,36 @@
 import json
-from string import Template
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 from templates import sel_script_template
 from templates import command_template
-from settings import BB_USERNAME
-from settings import BB_PASSWORD
-
-sel_script = Template(command_template)
-
-# repo_info = { 'repo_url':'myurl'}#, 'repo_name':'myname' }
-# print sel_script.substitute(repo_info)
-
-# make call to BB
-# see if it has parameter next
-# abort if not
-# from received data get repo urls.
+from settings import *
 
 def make_rest_call(url):
-	auth = HTTPBasicAuth(BB_USERNAME, BB_PASSWORD)
+	auth = HTTPBasicAuth(SRC_BB_USERNAME, SRC_BB_PASSWORD)
 	return requests.get(url=url, auth=auth).json()
 
-seed_url = 'https://bitbucket.org/api/2.0/repositories/virtuallabs'
+seed_url = 'https://bitbucket.org/api/2.0/repositories/' + SRC_BB_USERNAME
 response = make_rest_call(seed_url)
-urls = []
+bb_repo_urls = []
 
 while('next' in response.keys()):
 	for repo in response['values']:
-		urls.append(repo['links']['html']['href'])
+		bb_repo_urls.append(repo['links']['html']['href'])
 	response = make_rest_call(response['next'])
+	break
 
-print urls
+commands = "".join(map(lambda url: command_template.substitute({
+											'bb_username': SRC_BB_USERNAME, 
+											'bb_password': SRC_BB_PASSWORD, 
+											'repo_url': url,
+											'owner_value': IMPORTER_ID
+											}), bb_repo_urls))
+
+with open('selenium-scripts.txt', 'w') as sel_file: 
+	sel_file.write(sel_script_template.substitute({
+												'bb_username': IMPORTER_BB_USERNAME, 
+												'bb_password': IMPORTER_BB_PASSWORD, 
+												'commands': commands
+												}))
